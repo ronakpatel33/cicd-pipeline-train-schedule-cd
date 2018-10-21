46e8b33da443ace90ac33313d7681108ad27e96b
ronakpatel33 <44053373+ronakpatel33@users.noreply.github.com>
2018-10-21 20:16:03 +0530

    Update copy_artifacts.sh

--- ansible-scripts/copy_artifacts.sh
+++ ansible-scripts/copy_artifacts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 host_file=/etc/ansible/hosts
-ansible-playbook -i $host_file --private-key=/var/lib/jenkins/.ssh/id_rsa.pem site.yml
+ansible-playbook -i $host_file -e 'host_key_checking=False' site.yml
