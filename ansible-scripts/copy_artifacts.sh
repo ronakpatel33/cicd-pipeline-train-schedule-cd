461bb4ab88bda3a57bca9bb771d55c14e7fa57a2
ronakpatel33 <44053373+ronakpatel33@users.noreply.github.com>
2018-10-21 21:18:14 +0530

    Update copy_artifacts.sh

--- ansible-scripts/copy_artifacts.sh
+++ ansible-scripts/copy_artifacts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 host_file=/etc/ansible/hosts
-ansible-playbook -i $host_file -e 'host_key_checking=False' site.yml
+ansible-playbook -i $host_file -e 'host_key_checking=False' -u ec2-user site.yml
